621ca7ed568013cedfb691fc1fe578a0ffafb0f2
d0422 <rlfehd2013@naver.com>
2023-02-21 03:47:16 +0900

    input and event for create div

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,2 +1,8 @@
 import Div from './Div.mjs';
-const div = new Div('hi');
+const input = document.querySelector('input');
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    new Div(input.value);
+    input.value = '';
+  }
+});
